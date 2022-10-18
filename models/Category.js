f0d981bc63159f8888a2438bf4df94076ae61c5d
tayyjohnson <taydriver1@gmail.com>
2022-10-17 22:13:15 -0400

    working code but having my own computer issues

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,7 @@
 
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../../config/connection.js');
+const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
